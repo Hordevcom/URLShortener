677031326e2f81b149b2efe26c4c056746d7d8a5
Viel <vielavernus@gmail.com>
2025-01-31 14:06:32 +0300

    add config package

---
+++ cmd/shortener/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "flag"
+
+type Config struct {
+	ServerAdress string
+	Host         string
+}
+
+func GetCLParams() Config {
+	var conf Config
+	flag.StringVar(&conf.ServerAdress, "a", "localhost:8080", "server adress")
+	flag.StringVar(&conf.Host, "b", "localhost:8080", "host")
+
+	flag.Parse()
+	return conf
+}
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,10 +6,12 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/Hordevcom/URLShortener/cmd/shortener/config"
 	"github.com/gin-gonic/gin"
 )
 
 var urlStore = make(map[string]string)
+var conf config.Config
 
 func shortenURL(ctx *gin.Context) {
 	url, err := io.ReadAll(ctx.Request.Body)
@@ -17,7 +19,6 @@ func shortenURL(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err})
 	}
-	fmt.Println(string(url))
 
 	if string(url) == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": "url param required"})
@@ -26,7 +27,7 @@ func shortenURL(ctx *gin.Context) {
 
 	shortURL := fmt.Sprintf("%x", md5.Sum([]byte(url)))[:8]
 	urlStore[shortURL] = string(url)
-	ctx.String(http.StatusCreated, "http://localhost:8080/"+shortURL)
+	ctx.String(http.StatusCreated, "http://"+conf.Host+"/"+shortURL)
 }
 
 func redirect(ctx *gin.Context) {
@@ -42,11 +43,12 @@ func redirect(ctx *gin.Context) {
 }
 
 func main() {
+	conf = config.GetCLParams()
 
 	server := gin.Default()
 	server.HandleMethodNotAllowed = true
 	server.POST(`/`, shortenURL)
 	server.GET(`/:id`, redirect)
 
-	server.Run("localhost:8080")
+	server.Run(conf.ServerAdress)
 }
