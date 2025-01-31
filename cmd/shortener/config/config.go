0ca0b338897f8808277c4bf500757cdb5b36e91d
Viel <vielavernus@gmail.com>
2025-01-31 15:49:29 +0300

    add env var check

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -1,14 +1,29 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type Config struct {
-	ServerAdress string
-	Host         string
+	ServerAdress string `env:"SERVER_ADDRESS"`
+	Host         string `env:"BASE_URL"`
 }
 
 func GetCLParams() Config {
 	var conf Config
+	err := env.Parse(&conf)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if conf.Host != "" && conf.ServerAdress != "" {
+		return conf
+	}
+
 	flag.StringVar(&conf.ServerAdress, "a", "localhost:8080", "server adress")
 	flag.StringVar(&conf.Host, "b", "http://localhost:8080", "host")
 
