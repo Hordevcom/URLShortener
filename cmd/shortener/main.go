2a6dd25c01fca9f7c8bc09b51666d3cf6fc01c60
Viel <vielavernus@gmail.com>
2025-02-13 19:42:32 +0300

    add logging and remove url validation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,14 +5,18 @@ import (
 
 	"github.com/Hordevcom/URLShortener/internal/app"
 	"github.com/Hordevcom/URLShortener/internal/config"
+	"github.com/Hordevcom/URLShortener/internal/middleware/logging"
 	"github.com/Hordevcom/URLShortener/internal/routes"
 	"github.com/Hordevcom/URLShortener/internal/storage"
 )
 
 func main() {
+	logger := logging.NewLogger()
 	strg := storage.NewStorage()
 	conf := config.NewConfig()
 	app := app.NewApp(strg, conf)
-	router := routes.NewRouter(*app)
+	router := routes.NewRouter(*app, logger)
+
+	logger.Infow("Starting server", "addr", conf.ServerAdress)
 	http.ListenAndServe(conf.ServerAdress, router)
 }
