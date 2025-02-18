f3f03aca1f54598ecb1fa5d6327cceafea55f188
Viel <vielavernus@gmail.com>
2025-02-18 21:09:28 +0300

    change logic in config for all positions

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/Hordevcom/URLShortener/internal/middleware/logging"
 	"github.com/caarlos0/env/v11"
 )
 
@@ -15,26 +14,25 @@ type Config struct {
 }
 
 func NewConfig() Config {
-	logger := logging.NewLogger()
+
 	var conf Config
 	err := env.Parse(&conf)
 
-	logger.Infow(conf.FilePath)
-
 	if err != nil {
 		fmt.Println(err)
-	}
-
-	if conf.Host != "" && conf.ServerAdress != "" {
-		return conf
 	}
 
 	if conf.FilePath == "" {
 		flag.StringVar(&conf.FilePath, "f", "storage.txt", "path to file")
 	}
 
-	flag.StringVar(&conf.ServerAdress, "a", "localhost:8080", "server adress")
-	flag.StringVar(&conf.Host, "b", "http://localhost:8080", "host")
+	if conf.Host == "" {
+		flag.StringVar(&conf.Host, "b", "http://localhost:8080", "host")
+	}
+
+	if conf.ServerAdress == "" {
+		flag.StringVar(&conf.ServerAdress, "a", "localhost:8080", "server adress")
+	}
 
 	flag.Parse()
 	return conf
