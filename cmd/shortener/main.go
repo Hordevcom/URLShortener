d96b8d2c4b2ef81d2bbe084d128708744a1cbf6f
Viel <vielavernus@gmail.com>
2025-05-21 17:29:10 +0300

    global var for versions

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,7 +18,18 @@ import (
 	"github.com/Hordevcom/URLShortener/internal/storage"
 )
 
+// Переменные для версии сборки
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Println("Build version:", buildVersion)
+	fmt.Println("Build date:", buildDate)
+	fmt.Println("Build commit:", buildCommit)
+
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
